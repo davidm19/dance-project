0dc9b567fb96693a40fc5f463f59e9e254dddd44
kurzweilgenius <dmalone@chadwickschool.org>
2016-10-13 12:11:01 -0700

    change from swing to tetris theme

--- dance-project.py
+++ dance-project.py
@@ -2,9 +2,7 @@ from Myro import *
 init("sim")
 
 def dance(movement):
-    movement == 5
-while True:
-      movement
+    return movement
     
-cantina = [forward(5,.8), backward(5,.8), turnLeft(15,.8), turnRight(15,.8)]
-dance(cantina)s
+tetris = [forward(5,.5), ]
+dance(tetris)
