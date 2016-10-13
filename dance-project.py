2d6c24054640b95ede2f81126803b15a8dc54e95
kurzweilgenius <dmalone@chadwickschool.org>
2016-10-13 11:57:05 -0700

    first commits to project

---
+++ dance-project.py
@@ -0,0 +1,10 @@
+from Myro import *
+init("sim")
+
+def dance(movement):
+    movement == 5
+while True:
+      movement
+    
+cantina = [forward(5,.8), backward(5,.8), turnLeft(15,.8), turnRight(15,.8)]
+dance(cantina)s
