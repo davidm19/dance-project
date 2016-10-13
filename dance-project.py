63e176c758dbb934a8b9ab1dbc1b195fc3509617
kurzweilgenius <dmalone@chadwickschool.org>
2016-10-13 12:35:12 -0700

    Added code for the different sections of the song

--- dance-project.py
+++ dance-project.py
@@ -4,5 +4,17 @@ init("sim")
 def dance(movement):
     return movement
     
-tetris = [forward(5,.5), ]
-dance(tetris)
+tetrisFast25 = [forward(2,1.25), stop(), backward(2,1.25), stop()]
+dance(tetrisFast25)
+
+tetrisSlow13 = []
+dance(tetrisSlow13)
+
+tetrisFast27 = []
+dance(tetrisFast27)
+
+tetrisSlow12 = []
+dance(tetrisSlow12)
+
+tetrisFast6 = []
+dance(tetrisFast6)
