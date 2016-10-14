b40f89cb0b3603d3ff345eb6033c59ae8e44e316
kurzweilgenius <dmalone@chadwickschool.org>
2016-10-14 11:33:53 -0700

    Make the list that joins the two halfs of first fast dance together

--- dance-project.py
+++ dance-project.py
@@ -4,7 +4,9 @@ init("sim")
 def dance(movement):
     return movement
     
-tetrisFast25 = [forward(3,1.25), stop(), turnLeft(3,1.25), forward(3,1.25), turnRight(10,3), backward(3,1.25), stop(), turnLeft(20,1), forward(3,1.25), stop(), turnRight(10,1), backward(3,1.25)]
+tetrisFast25Half1 = [forward(3,1.25), stop(), turnLeft(3,1.25), forward(3,1.25), turnRight(10,3), backward(3,1.25), stop(), turnLeft(20,1), forward(3,1.25), stop(), turnRight(10,1), backward(3,1.25), stop()]
+tetrisFast25Half2 = [forward(3,1.25), stop(), turnLeft(3,1.25), forward(3,1.25), turnRight(10,3), backward(3,1.25), stop(), turnLeft(20,1), forward(3,1.25), stop(), turnRight(10,1), backward(3,1.25), stop()]
+tetrisFast25 = {tetrisFast25Half1, tetrisFast25Half2}
 dance(tetrisFast25)
 
 tetrisSlow13 = []
