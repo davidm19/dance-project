c3163c74f5ec4d62de7988456eb13b2ab5480779
kurzweilgenius <dmalone@chadwickschool.org>
2016-10-17 09:07:18 -0700

    Add 2nd fast part dance moves

--- dance-project.py
+++ dance-project.py
@@ -10,15 +10,13 @@ def dance(movement):
 #dance(tetrisFast25)
 
 tetrisSlow13Half1 = [forward(3,.75), turnRight(1.5,.75), backward(3,.75), turnLeft(2,.75), backward(3,.75), backward(2,.75), turnRight(3.5,.75)]
-tetrisSlow13Half2 = [forward(3,.75), turnRight(1.5,.75), backward(3,.75), turnLeft(2,.75), backward(3,.75), turnLeft(2,.25), turnRight(2,.25), turnLeft(2,.25), backward(3,.5), forward(4,1)]
+tetrisSlow13Half2 = [forward(3,.75), turnRight(1.5,.75), backward(3,.75), turnLeft(2,.75), backward(3,.75), turnLeft(2,.25), turnRight(2,.25), turnLeft(2,.25), backward(3,.5), forward(4,1), stop()]
 tetrisSlow13 = {tetrisSlow13Half1, tetrisSlow13Half2}
 dance(tetrisSlow13)
 
-tetrisFast27 = []
-dance(tetrisFast27)
+dance(tetrisFast25)
 
-tetrisSlow12 = []
-dance(tetrisSlow12)
+dance(tetrisSlow13)
 
 tetrisFast6 = []
 dance(tetrisFast6)
