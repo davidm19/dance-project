2222c60e02b591e61d0d5c920661221e4e3a0248
kurzweilgenius <dmalone@chadwickschool.org>
2016-10-17 09:02:00 -0700

    Add slow tetris dance moves

--- dance-project.py
+++ dance-project.py
@@ -4,12 +4,14 @@ init("sim")
 def dance(movement):
     return movement
     
-tetrisFast25Half1 = [forward(3,1.25), stop(), turnLeft(3,1.25), forward(3,1.25), turnRight(10,3), backward(3,1.25), stop(), turnLeft(20,1), forward(3,1.25), stop(), turnRight(10,1), backward(3,1.25), stop()]
-tetrisFast25Half2 = [forward(3,1.25), stop(), turnLeft(3,1.25), forward(3,1.25), turnRight(10,3), backward(3,1.25), stop(), turnLeft(20,1), forward(3,1.25), stop(), turnRight(10,1), backward(3,1.25), stop()]
-tetrisFast25 = {tetrisFast25Half1, tetrisFast25Half2}
-dance(tetrisFast25)
+#tetrisFast25Half1 = [forward(3,1.25), stop(), turnLeft(3,1.25), forward(3,1.25), turnRight(10,3), backward(3,1.25), stop(), turnLeft(20,1), forward(3,1.25), stop(), turnRight(10,1), backward(3,1.25), stop()]
+#tetrisFast25Half2 = [forward(3,1.25), stop(), turnLeft(3,1.25), forward(3,1.25), turnRight(10,3), backward(3,1.25), stop(), turnLeft(20,1), forward(3,1.25), stop(), turnRight(10,1), backward(3,1.25), stop()]
+#tetrisFast25 = {tetrisFast25Half1, tetrisFast25Half2}
+#dance(tetrisFast25)
 
-tetrisSlow13 = []
+tetrisSlow13Half1 = [forward(3,.75), turnRight(1.5,.75), backward(3,.75), turnLeft(2,.75), backward(3,.75), backward(2,.75), turnRight(3.5,.75)]
+tetrisSlow13Half2 = [forward(3,.75), turnRight(1.5,.75), backward(3,.75), turnLeft(2,.75), backward(3,.75), turnLeft(2,.25), turnRight(2,.25), turnLeft(2,.25), backward(3,.5), forward(4,1)]
+tetrisSlow13 = {tetrisSlow13Half1, tetrisSlow13Half2}
 dance(tetrisSlow13)
 
 tetrisFast27 = []
