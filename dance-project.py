6b8db5dd67bd40b48bb0c090e300b09f0b2f4743
davidm19 <dmalone@chadwickschool.org>
2016-10-18 09:51:25 -0700

    Update dance-project.py

--- dance-project.py
+++ dance-project.py
@@ -1,28 +1,53 @@
 from Myro import *
 init("sim")
 
-def tetrisFast25Half():
+def tetrisFast25():
     forward(3,1.25)
     stop()
     turnLeft(3,1.25)
+    forward(3,1.25)
+    turnRight(10,3)
+    backward(3,1.25)
+    stop()
+    turnLeft(20,1)
+    forward(3,1.25)
+    stop(), turnRight(10,1)
+    backward(3,1.25)
+    stop()
+
+tetrisFast25() #put it in a loop
+tetrisFast25()
+
+def tetrisSlow13():
+    forward(3,.75)
+    turnRight(1.5,.75)
+    backward(3,.75)
+    turnLeft(2,.75)
+    backward(3,.75)
+    backward(2,.75)
+    turnRight(3.5,.75)
     
-        
-#tetrisFast25Half1 = [forward(3,1.25), stop(), turnLeft(3,1.25), forward(3,1.25), turnRight(10,3), backward(3,1.25), stop(), turnLeft(20,1), forward(3,1.25), stop(), turnRight(10,1), backward(3,1.25), stop()]
-#tetrisFast25Half2 = [forward(3,1.25), stop(), turnLeft(3,1.25), forward(3,1.25), turnRight(10,3), backward(3,1.25), stop(), turnLeft(20,1), forward(3,1.25), stop(), turnRight(10,1), backward(3,1.25), stop()]
-#tetrisFast25 = {tetrisFast25Half1, tetrisFast25Half2}
-#dance(tetrisFast25)
+tetrisSlow13()
+tetrisSlow13()
 
-tetrisFast25Half() #put it in a loop
-tetrisFast25Half()
+tetrisFast25()
 
-## tetrisSlow13Half1 = [forward(3,.75), turnRight(1.5,.75), backward(3,.75), turnLeft(2,.75), backward(3,.75), backward(2,.75), turnRight(3.5,.75)]
-## tetrisSlow13Half2 = [forward(3,.75), turnRight(1.5,.75), backward(3,.75), turnLeft(2,.75), backward(3,.75), turnLeft(2,.25), turnRight(2,.25), turnLeft(2,.25), backward(3,.5), forward(4,1), stop()]
-## tetrisSlow13 = {tetrisSlow13Half1, tetrisSlow13Half2}
-## dance(tetrisSlow13)
-## 
-## dance(tetrisFast25)
-## 
-## dance(tetrisSlow13)
-## 
-## tetrisFast6 = [forward(3,.5), turnLeft(5,.2), turnRight(5,.2), turnLeft(5,.2), backward(4,.2), backward(4,.2), backward(4,.2), turnLeft(3,.2), turnRight(3,.2), turnLeft(7.5,.2), backward(4,1), turnLeft(3,.2), turnRight(3,.2), forward(2,2.5)]
-## dance(tetrisFast6)
+tetrisSlow13()
+
+def tetrisFast6():
+    forward(3,.5)
+    turnLeft(5,.2)
+    turnRight(5,.2)
+    turnLeft(5,.2)
+    backward(4,.2)
+    backward(4,.2)
+    backward(4,.2)
+    turnLeft(3,.2)
+    turnRight(3,.2)
+    turnLeft(7.5,.2)
+    backward(4,1)
+    turnLeft(3,.2)
+    turnRight(3,.2)
+    forward(2,2.5)
+
+tetrisFast6()
